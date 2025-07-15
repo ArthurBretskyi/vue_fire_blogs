7d5ef5ff2372bc3ac20d8d7889e970be0977786c
ArthurBretskyi <mts2011315@gmail.com>
2025-07-15 18:06:37 +0300

    firebaseConfigChanges

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBXSN8D_dzZiHUYAwrD7SfzQxqsJCd5X6Q',
-    authDomain: 'vuefireblogsab.firebaseapp.com',
-    projectId: 'vuefireblogsab',
-    storageBucket: 'vuefireblogsab.firebasestorage.app',
-    messagingSenderId: '897014757795',
-    appId: '1:897014757795:web:f50b7a38e75bb62db50216',
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
